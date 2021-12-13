b3ce4546df1bede1bbd1b64d53d3e1872eb7ffd1
Angron42 <bmaruhnenko@gmail.com>
2021-12-13 22:04:03 +0200

    Fix the web_server.py file

--- web_server.py
+++ web_server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_file, send_from_directory
 from python_modules.config_parser import get_config
 
 app = Flask(__name__)
@@ -9,12 +9,30 @@ config = get_config('web_server.config', {
     }
 })
 
+@app.route('/<path:path>')
+def send_static_file(path):
+    index_of_dot = path.rfind('.')
+    extension = path[index_of_dot + 1:]
+    content_type = 'text/plain'
+
+    content_types = {
+        'js': 'text/javascript',
+        'css': 'text/css'
+    }
+
+    if extension in content_types:
+        content_type = content_types[extension]
+
+    return send_from_directory('static', path, mimetype=content_type)
+
 @app.route('/')
 def main_page():
-    return '<h1>Test</h1>'
+    return send_file('index.html')
     
 if __name__ == '__main__':
     app.run(
         host = config['WEB_SERVER']['Host'],
         port = config['WEB_SERVER']['Port']
     )
+
+print("12345"[-2:])
