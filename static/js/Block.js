eb147f72b286278bbd98efc49784f40eb172511c
Angron42 <bmaruhnenko@gmail.com>
2021-12-19 17:50:26 +0200

    Blocks now can be updated

--- static/js/Block.js
+++ static/js/Block.js
@@ -1,1 +1,25 @@
-export default class Block {}
+import EventHandler from './Event.js'
+
+export default class Block {
+    #event = new EventHandler()
+    
+    #updateGravity(type, worldData, x, y) {
+        if (type !== 'block' || !this.properties.hasGravity || y + 1 >= worldData.height) {
+            return false
+        }
+    
+        if (!worldData.worldMatrix[x][y + 1].block) {
+            worldData.moveBlock(x, y, x, y + 1)
+
+            if (y + 2 < worldData.height && worldData.worldMatrix[x][y + 2].block) {
+                worldData.updateNearestBlocks(x, y + 2)
+            }
+        }
+    }
+
+    constructor() {
+        this.event.addEventListener('update', this.#updateGravity.bind(this))
+    }
+
+    get event() { return this.#event }
+}
