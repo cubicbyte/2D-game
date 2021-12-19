9b9178888adda13863301c5d8b9eed4978140693
Angron42 <bmaruhnenko@gmail.com>
2021-12-19 12:58:57 +0200

    Some little changes

--- static/js/Camera.js
+++ static/js/Camera.js
@@ -32,9 +32,7 @@ export default class Camera {
                 })
             }
             
-            requestAnimationFrame(() => {
-                this.#render()
-            })
+            requestAnimationFrame(this.#render.bind(this))
         }
 
         static addRenderer(renderer) {
