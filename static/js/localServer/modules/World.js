8966d3bd9099c96e4a34f222122ed35d012d3b14
Angron42 <bmaruhnenko@gmail.com>
2022-01-19 17:58:07 +0200

    World registration moved to separate file

--- static/js/localServer/modules/World.js
+++ static/js/localServer/modules/World.js
@@ -1,17 +1,7 @@
+import Registration from './Registration.js'
 import WorldData from './WorldData.js'
 
 export default class World {
     WorldData = new WorldData()
-
-    #id = null
-    #registered = false
-
-    register(id) {
-        this.#id = id
-        this.#registered = true
-        delete this.register
-    }
-
-    get id() { return this.#id }
-    get registered() { return this.#registered }
+    Registration = new Registration()
 }
