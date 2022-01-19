2f3f4e35435abb5176081d579a9cef6c63981bd7
Angron42 <bmaruhnenko@gmail.com>
2022-01-19 14:41:27 +0200

    Created player entity

---
+++ static/js/localServer/modules/Player.js
@@ -0,0 +1,25 @@
+import Entity from './Entity.js'
+import CommunicationInterface from './CommunicationInterface.js'
+import { validateString } from './dataValidator.js'
+
+export default class Player extends Entity {
+    CommunicationInterface = new CommunicationInterface()
+
+    #name = null
+
+    constructor(name = null) {
+        this.name = name
+    }
+
+    get name() { return this.#name }
+
+    set name(value) {
+        if (value === null) {
+            this.#name = value
+            return
+        }
+        
+        validateString(value, 'Player name')
+        this.#name = value
+    }
+}
